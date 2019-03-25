b299c235ac8bfa6c317e7c9ed0f43537292714c5
Famio <famio@qq.com>
2019-03-25 22:43:52 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-container>
+    <el-header>
+      <el-row>
+        <el-col :span="24">
+          <div class="grid-content bg-purple-dark">
+            <el-menu
+              :default-active="activeIndex"
+              class="el-menu-demo"
+              mode="horizontal"
+              @select="handleSelect"
+            >
+              <el-menu-item index="1">HOME</el-menu-item>
+              <el-menu-item index="2">CODING</el-menu-item>
+              <el-menu-item index="3">DAIRY</el-menu-item>
+            </el-menu>
+            <div class="line"></div>
+          </div>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-main>
+      <router-view/>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      activeIndex: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
